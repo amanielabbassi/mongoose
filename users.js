364f6e151851b4c45092fd8f86d2f76a2abe0716
amanielabbassi <lyemnaabassi@gmail.com>
2021-10-21 06:21:34 +0100

    firstcommit

---
+++ users.js
@@ -0,0 +1,17 @@
+const users = [
+    {
+      id:2,
+  name: "amani abbassi",
+    age: 24,
+    favoriteFoods: ["riz", "chapati"], },
+    {id:3,
+      name: "mouna abbassi",
+    age: 28,
+    favoriteFoods: ["makarouna", "fast-food"],},
+    {id:4,
+      name: "fatma brahmi",
+    age: 2,
+    favoriteFoods: ["makarouna", "riz"],}
+    
+];
+module.exports = users;
